45c9ef16cf66636bafa691c1fa39390c541bccc7
Nagula Ankitha <nankitha46@gmail.com>
2022-02-17 17:43:14 +0530

    Add files via upload

---
+++ 3_Implementation/header/calculator.h
@@ -0,0 +1,35 @@
+/**
+ * @file calculator.c
+ * @author nagula ankitha
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef PROJECT_H_
+#define PROJECT_H_
+
+
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+
+/**
+ * @brief 
+ * 
+ * @param X
+ * @return int 
+ */
+
+
+  /**
+     * @brief 
+     * 
+     * @param n
+     * @param tot
+     * @param k
+     * @param number
+     * @return int
+     */  
